df622692d50cc30cc8985d0df6671538aa7ad2ab
Dang Tuan <dangminhtuan.arwen@gmail.com>
2021-04-09 15:23:40 +0700

    [Thực hành] Hệ các đối tượng hình học

---
+++ thucHanh/Square.java
@@ -0,0 +1,31 @@
+package thucHanh;
+
+public class Square extends Rectangle {
+    public Square() {
+    }
+
+    public Square(double side) {
+        super(side, side);
+    }
+
+    public Square(String color, Boolean filled, double side) {
+        super(color, filled, side, side);
+    }
+
+    public double getSide() {
+        return getLength();
+    }
+
+    public void setSide(double side) {
+        setLength(side);
+        setWidth(side);
+    }
+
+    @Override
+    public String toString() {
+        return "A Square with side="
+                + getSide()
+                + ", which is a subclass of "
+                + super.toString();
+    }
+}
