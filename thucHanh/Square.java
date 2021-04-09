1a064fcc1168591edca094aff63f26768030b6d2
Dang Tuan <dangminhtuan.arwen@gmail.com>
2021-04-09 15:25:44 +0700

    [Thực hành] Hệ các đối tượng hình học

--- thucHanh/Square.java
+++ thucHanh/Square.java
@@ -22,6 +22,16 @@ public class Square extends Rectangle {
     }
 
     @Override
+    public void setWidth(double width) {
+        setSide(width);
+    }
+
+    @Override
+    public void setLength(double length) {
+        setSide(length);
+    }
+
+    @Override
     public String toString() {
         return "A Square with side="
                 + getSide()
